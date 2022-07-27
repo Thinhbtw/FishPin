5cd4f38a252df74022fe0e80d4fce68d3c4813ed
Thinhbtw <ducthinhbtw@gmail.com>
2022-07-27 15:11:03 +0700

    firebase

---
+++ Assets/Script/Firebase/FirebaseInit.cs
@@ -0,0 +1,20 @@
+using Firebase;
+using Firebase.Analytics;
+using UnityEngine;
+
+public class FirebaseInit : Singleton<FirebaseInit>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+        });
+    }
+
+    public void EventAds()
+    {
+        
+    }
+}
